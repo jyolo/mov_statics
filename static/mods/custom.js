e4509c4e0ab7a4410186a696cd5a65a952102b75
2829156353@qq.com <2829156353@qq.com>
2020-07-24 14:31:57 +0800

    优化图片的懒加载

--- static/mods/custom.js
+++ static/mods/custom.js
@@ -1,3 +1,9 @@
+/**
+ * echo.min.js 图片懒加载
+ */
+﻿window.Echo=(function(window,document,undefined){'use strict';var store=[],offset,throttle,poll;var _inView=function(el){var coords=el.getBoundingClientRect();return((coords.top>=0&&coords.left>=0&&coords.top)<=(window.innerHeight||document.documentElement.clientHeight)+parseInt(offset));};var _pollImages=function(){for(var i=store.length;i--;){var self=store[i];if(_inView(self)){self.src=self.getAttribute('data-echo');store.splice(i,1);}}};var _throttle=function(){clearTimeout(poll);poll=setTimeout(_pollImages,throttle);};var init=function(obj){var nodes=document.querySelectorAll('[data-echo]');var opts=obj||{};offset=opts.offset||0;throttle=opts.throttle||250;for(var i=0;i<nodes.length;i++){store.push(nodes[i]);}_throttle();if(document.addEventListener){window.addEventListener('scroll',_throttle,false);}else{window.attachEvent('onscroll',_throttle);}};return{init:init,render:_throttle};})(window,document);
+
+
 var host = 'https://api.yy8.pw'
 var ws_host = 'wss://ws.yy8.pw'
 var cover_host = 'https://cdn.jsdelivr.net/gh/zenhiss/mov_img/cover/'
